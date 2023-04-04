d8c6422eda7808dc2bedebcbfb276a3618f9b42f
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-04-04 23:20:18 +0530

    server created

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ dotenv.config();
 
 const app = express();
 
+
 app.get("/",(req,resp)=>{
     resp.send("Api is Running successfully");
 })
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000,console.log(`server started on Port ${PORT}`));
