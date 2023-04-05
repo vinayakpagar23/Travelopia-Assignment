0d746fb7f39815b3d4142f4f90d9ba49c7b5f10b
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-04-05 13:53:59 +0530

    all fields working fine

--- client/src/components/DetailsForm.jsx
+++ client/src/components/DetailsForm.jsx
@@ -10,21 +10,21 @@ import {
   InputLeftElement,
   InputRightElement,
   CheckIcon,
-  useToast
+  useToast,
+  Text
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const DetailsForm = () => {
-  const [show, setShow] = useState(false);
-
   const [name, setName] = useState("vk");
   const [email, setEmail] = useState("ck");
   const [destination, setDestination] = useState();
   const [no_of_travellers, setNumTravellers] = useState(1);
   const [budgetPerPerson, setBudgetPerPerson] = useState(0);
+  const [totalBudget,setTotalBudget] = useState(0);
   const [picLoading, setPicLoading] = useState(false);
   const toast = useToast();
-  const submitHandler = async() => {
+  const submitHandler = async () => {
     setPicLoading(true);
     if (!name || !email || !destination || !no_of_travellers) {
       toast({
@@ -50,7 +50,8 @@ const DetailsForm = () => {
           email,
           destination,
           no_of_travellers,
-          budgetPerPerson
+          budgetPerPerson,
+          totalBudget
         },
         config
       );
@@ -64,10 +65,9 @@ const DetailsForm = () => {
       });
       localStorage.setItem("details", JSON.stringify(data));
       setPicLoading(false);
- 
     } catch (error) {
       toast({
-        title: "Error Occured!",
+        title: "Email Is already used, Please use another email!",
         description: error.response.data.message,
         status: "error",
         duration: 5000,
@@ -77,6 +77,29 @@ const DetailsForm = () => {
       setPicLoading(false);
     }
   };
+
+  const chooseCountry=(value)=>{
+    setDestination(value);
+    if(value==="India"){
+        setBudgetPerPerson(15000);
+    }
+    else if(value==="Africa"){
+      setBudgetPerPerson(20000);
+  }
+  else{
+    setBudgetPerPerson(25000);
+  }
+  }
+ 
+  const TBudget=(value)=>{
+    setNumTravellers(value);
+  
+    // setTotalBudget(budgetPerPerson*Number(no_of_travellers));
+  }
+
+useEffect(()=>{
+   setTotalBudget(budgetPerPerson*no_of_travellers)
+},[no_of_travellers])
   return (
     <Box width={"50%"} margin={"auto"}>
       <VStack spacing={"5px"} color="black">
@@ -96,7 +119,10 @@ const DetailsForm = () => {
         </FormControl>
         <FormControl id="destination" isRequired>
           <FormLabel>Destination</FormLabel>
-          <Select placeholder="Select country" onChange={(e)=>setDestination(e.target.value)}>
+          <Select
+            placeholder="Select country"
+            onChange={(e)=>chooseCountry(e.target.value)}
+          >
             <option value="India">India</option>
             <option value="Africa">Africa</option>
             <option value="Europe">Europe</option>
@@ -107,29 +133,38 @@ const DetailsForm = () => {
           <Input
             type="number"
             placeholder="Enter No. of travellers"
-            onChange={(e) => setNumTravellers(e.target.value)}
+            onChange={(e) =>TBudget(e.target.value) }
           />
         </FormControl>
 
         <FormControl id="budget" isRequired>
           <FormLabel>Budget Per Person</FormLabel>
-
-          <InputGroup>
+          <Text fontSize='2xl'>{budgetPerPerson}</Text>
+          {/* <InputGroup>
             <InputLeftElement
               pointerEvents="none"
               color="gray.300"
               fontSize="1.2em"
               children="$"
             />
-            <Input placeholder="Enter amount" type="number" onChange={(e)=>setBudgetPerPerson(e.target.value)} />
-          </InputGroup>
-        </FormControl>
+            <Input
+              placeholder="Enter amount"
+              type="number"
+              onChange={(e) => setBudgetPerPerson(e.target.value)}
+            /> */}
+          {/* </InputGroup> */}
+          </FormControl>
+        <FormControl id="Total budget" isRequired>
+          <FormLabel>Total Budget</FormLabel>
+          <Text fontSize='2xl'>{totalBudget}</Text>
+          </FormControl>
 
         <Button
           colorScheme="blue"
           width="100%"
           style={{ marginTop: 15 }}
           onClick={submitHandler}
+          isLoading={picLoading}
         >
           Submit Data
         </Button>
